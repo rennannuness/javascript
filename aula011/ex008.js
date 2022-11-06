6786c7f4dbf93f8460269b0feca45e8cf572176a
Rennan Nunes <111397688+rennannuness@users.noreply.github.com>
2022-11-06 14:09:11 -0300

    atividades

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 65
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) { // condição simples!
+    console.log(`Você ultrapassou a velocidade permitida.Multado!`)
+}
+console.log(`Dirija sempre utiliando o cinto de segurança!`)
